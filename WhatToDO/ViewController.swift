5e211fca96ae769edf0707db5be7d9e43a4a6200
Tiago Do Couto <cs.couto@outlook.com>
2018-01-15 11:40:00 -0600

    Initial Commit

---
+++ WhatToDO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatToDO
+//
+//  Created by Tiago Do Couto on 1/15/18.
+//  Copyright © 2018 Tiago Do Couto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
